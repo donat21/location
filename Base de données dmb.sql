4e5a9e05d2035ad63d39741cf49baf4eb7d91247
donat21 <donatshilenge@gmail.com>
2016-08-24 07:00:34 +0200

    first comm

---
+++ Base de données dmb.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Ven 15 Juillet 2016 à 11:03
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `football`
+--
+
+-- --------------------------------------------------------
+--
+-- Structure de la table `users`
+--
+drop table if exists users;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3;
+
+
+drop table if exists draws;
+
+CREATE TABLE IF NOT EXISTS draws(
+
+id int NOT NULL AUTO_INCREMENT,
+title varchar(30) ,
+picturepath varchar(30) not null,
+date datetime,
+
+PRIMARY KEY (Id)
+);
+
+ALTER TABLE users
+    ADD COLUMN lastname VARCHAR(50) NOT NULL AFTER password,
+    ADD COLUMN firstname VARCHAR(50) NOT NULL
+    AFTER password
